6a488d24c32dbf0171e8d7f85fa5088adb188cfc
Andrey Statkevskiy <andstatik@gmail.com>
2022-05-08 21:09:16 -0400

    db is seeded conditionally: if exercises collection is empty - it will be seeded; else will not

--- firebase.js
+++ firebase.js
@@ -1,15 +1,15 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getFirestore, addDoc, collection, getDocs, query } from "firebase/firestore";
+// import { seedDb } from './seed';
 import {
   getAuth,
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
   onAuthStateChanged,
 } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+// App's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCE3pzg38mjnVmtilI0F4LMZ6pb8r4dGIo",
   authDomain: "hiitcoin-d8648.firebaseapp.com",
@@ -20,8 +20,28 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-
-let app = initializeApp(firebaseConfig)
+const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const db = getFirestore();
 
-export { auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged };
+const colRef = collection(db, "Exercises");
+const q = query(collection(db, "Exercises"));
+// Seed the exercises. Won't seed if exercises collection contains exercises documents.
+async function seedDb() {
+  let length = 0; // dummy counter will reflect amount of docs in the collection
+  const exercisesSnap = await getDocs(q); // obtain snapshot of the exercises collection
+  exercisesSnap.forEach(() => { // for each item in the colletion...
+    length++;                   // ... increment lenght by 1
+  });
+  if (length === 0) { // if length remained 0, then db is not seeded, so we will seed it
+    addDoc(colRef, {
+      basePoints: 10,
+      bodyPart: 'full-body',
+      difficulty: 3,
+      name: 'Deadlift',
+    });
+  } else console.log("The exercises collection is already seeded, mate.")
+}
+seedDb();
+
+export { firebaseConfig, auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged };
