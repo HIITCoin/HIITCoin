093deeb05e0112aa39f9eb2940e35da9a5d94c92
Ethan Bradley <elmb201@gmail.com>
2022-05-11 08:14:00 -0700

    stashing changes

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,5 +1,5 @@
-import { Pressable, StyleSheet } from "react-native";
-import React, { useState, useEffect } from "react";
+import { Pressable, StyleSheet } from 'react-native';
+import React, { useState, useEffect } from 'react';
 import {
   KeyboardAvoidingView,
   Text,
@@ -7,18 +7,26 @@ import {
   Flex,
   Box,
   HStack,
-} from "native-base";
-import { MaterialIcons } from "@expo/vector-icons";
-import { useNavigation } from "@react-navigation/native";
-import { getDocs, collection, doc, query, setDoc } from "firebase/firestore";
-import { db } from "../firebase";
-import { getAuth, signOut } from "firebase/auth";
-import { addNewWorkout, createHistory, getExercises, getSingleExercise, getSingleWorkout, getUser, getUserWorkouts } from "../misc/helperFunctions";
+} from 'native-base';
+import { MaterialIcons } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+import { getDocs, collection, doc, query, setDoc } from 'firebase/firestore';
+import { db } from '../firebase';
+import { getAuth, signOut } from 'firebase/auth';
+import {
+  addNewWorkout,
+  createHistory,
+  getExercises,
+  getSingleExercise,
+  getSingleWorkout,
+  getUser,
+  getUserWorkouts,
+} from '../misc/helperFunctions';
 
 const HomeScreen = () => {
   const navigation = useNavigation();
   const auth = getAuth();
-  const exCollection = collection(db, "Exercises");
+  const exCollection = collection(db, 'Exercises');
 
   useEffect(() => {
     const getExer = async () => {
@@ -35,7 +43,7 @@ const HomeScreen = () => {
       const data = await getDocs(exCollection).then((snapshot) => {
         snapshot.docs.forEach((doc) => arr.push({ ...doc.data(), id: doc.id }));
       });
-      console.log(auth.currentUser.uid, auth.currentUser.email)
+      console.log(auth.currentUser.uid, auth.currentUser.email);
     };
     getExer();
   }, []);
@@ -43,125 +51,100 @@ const HomeScreen = () => {
   const handleSignOut = async () => {
     try {
       await signOut(auth);
-      console.log("Get out");
-      navigation.navigate("Login");
+      console.log('Get out');
+      navigation.navigate('Login');
     } catch (error) {
       alert(error.message);
     }
   };
-	return (
-		<KeyboardAvoidingView bg="colors.bg" height="100%">
-			<Box marginTop="20%" marginBottom="10%" >
-				<HStack justifyContent="space-between">
-					<Pressable
-						//implement navigation.navigate("where")
-						onPress={() => console.log("Home pressed")}
-					>
-						<MaterialIcons
-							name="home"
-							size={50}
-							color="#9067C6"
-						/>
-					</Pressable>
-					<Pressable
-						//implement navigation.navigate("where")
-						onPress={() => navigation.navigate("Profile")}
-					>
-						<MaterialIcons
-							name="person"
-							color="#9067C6"
-							size={50}
-						/>
-					</Pressable>
-				</HStack>
-				{/* Get userName from props/state/auth and implement here */}
-				<Text fontSize="6xl" color="colors.text">
-					Hello, User
-				</Text>
-			</Box>
-			<VStack space={4} alignItems="center" bg="colors.bg">
-				<Box
-					w="100%"
-					h="10"
-					bg="colors.bg"
-					rounded="md"
-					borderWidth="2px"
-					borderColor="colors.text"
-					shadow={3}
-					justifyContent="center"
-				>
-					<Pressable onPress={() => navigation.navigate("Profile")}>
-						<Text
-							fontSize="xl"
-							color="colors.text"
-							marginLeft="10px"
-						>
-							Profile
-						</Text>
-					</Pressable>
-				</Box>
-				<Box
-					w="100%"
-					h="10"
-					bg="colors.bg"
-					rounded="md"
-					borderWidth="2px"
-					borderColor="colors.text"
-					shadow={3}
-					justifyContent="center"
-				>
-					<Pressable onPress={() => console.log("Stats pressed")}>
-						<Text
-							fontSize="xl"
-							color="colors.text"
-							marginLeft="10px"
-						>
-							Stats
-						</Text>
-					</Pressable>
-				</Box>
-				<Box
-					w="100%"
-					h="10"
-					bg="colors.bg"
-					rounded="md"
-					borderWidth="2px"
-					borderColor="colors.text"
-					shadow={3}
-					justifyContent="center"
-				>
-					<Pressable onPress={() => console.log("Workouts pressed")}>
-						<Text
-							fontSize="xl"
-							color="colors.text"
-							marginLeft="10px"
-						>
-							Workout!
-						</Text>
-					</Pressable>
-				</Box>
-				<Box
-					w="100%"
-					h="10"
-					bg="colors.bg"
-					rounded="md"
-					borderWidth="2px"
-					borderColor="colors.text"
-					shadow={3}
-					justifyContent="center"
-				>
-					<Pressable
-						onPress={() => console.log("Quick Timer pressed")}
-					>
-						<Text
-							fontSize="xl"
-							color="colors.text"
-							marginLeft="10px"
-						>
-							Quick Timer
-						</Text>
-					</Pressable>
-				</Box>
+
+  return (
+    <KeyboardAvoidingView bg="colors.bg" height="100%">
+      <Box marginTop="20%" marginBottom="10%">
+        <HStack justifyContent="space-between">
+          <Pressable
+            //implement navigation.navigate("where")
+            onPress={() => console.log('Home pressed')}
+          >
+            <MaterialIcons name="home" size={50} color="#9067C6" />
+          </Pressable>
+          <Pressable
+            //implement navigation.navigate("where")
+            onPress={() => navigation.navigate('Profile')}
+          >
+            <MaterialIcons name="person" color="#9067C6" size={50} />
+          </Pressable>
+        </HStack>
+        {/* Get userName from props/state/auth and implement here */}
+        <Text fontSize="6xl" color="colors.text">
+          Hello, User
+        </Text>
+      </Box>
+      <VStack space={4} alignItems="center" bg="colors.bg">
+        <Box
+          w="100%"
+          h="10"
+          bg="colors.bg"
+          rounded="md"
+          borderWidth="2px"
+          borderColor="colors.text"
+          shadow={3}
+          justifyContent="center"
+        >
+          <Pressable onPress={() => navigation.navigate('Profile')}>
+            <Text fontSize="xl" color="colors.text" marginLeft="10px">
+              Profile
+            </Text>
+          </Pressable>
+        </Box>
+        <Box
+          w="100%"
+          h="10"
+          bg="colors.bg"
+          rounded="md"
+          borderWidth="2px"
+          borderColor="colors.text"
+          shadow={3}
+          justifyContent="center"
+        >
+          <Pressable onPress={() => console.log('Stats pressed')}>
+            <Text fontSize="xl" color="colors.text" marginLeft="10px">
+              Stats
+            </Text>
+          </Pressable>
+        </Box>
+        <Box
+          w="100%"
+          h="10"
+          bg="colors.bg"
+          rounded="md"
+          borderWidth="2px"
+          borderColor="colors.text"
+          shadow={3}
+          justifyContent="center"
+        >
+          <Pressable onPress={() => navigation.navigate('Workouts')}>
+            <Text fontSize="xl" color="colors.text" marginLeft="10px">
+              Workout!
+            </Text>
+          </Pressable>
+        </Box>
+        <Box
+          w="100%"
+          h="10"
+          bg="colors.bg"
+          rounded="md"
+          borderWidth="2px"
+          borderColor="colors.text"
+          shadow={3}
+          justifyContent="center"
+        >
+          <Pressable onPress={() => console.log('Quick Timer pressed')}>
+            <Text fontSize="xl" color="colors.text" marginLeft="10px">
+              Quick Timer
+            </Text>
+          </Pressable>
+        </Box>
         <Box
           w="100%"
           h="10"
