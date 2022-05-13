97001fa7bbe00178ab2431ec375de251c93cc972
Mike <lebegue92@yahoo.com>
2022-05-13 14:50:06 -0500

    timer wont rest bug

--- screens/Timer.js
+++ screens/Timer.js
@@ -5,19 +5,49 @@ import { FontAwesome } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
 import { addNewWorkout, getSingleWorkout } from "../misc/helperFunctions";
 
+const workout = {
+  name: "testworkout",
+  exercises: [
+    {
+      name: "Leg Press",
+      basePoints: 10,
+      bodyPart: "legs",
+      difficulty: 3,
+      duration: 10,
+      reps: 8,
+      sets: 3,
+      rest: 5,
+    },
+    {
+      name: "Chest Press",
+      basePoints: 10,
+      bodyPart: "chest",
+      difficulty: 2,
+      duration: 10,
+      reps: 4,
+      sets: 3,
+      rest: 5,
+    },
+  ],
+  rounds: 3,
+  roundRest: 10,
+};
+//        (props)
 const Timer = () => {
   // obtain workout prop from the workout component
   const navigation = useNavigation();
-
-  const [myWorkout, setMyWorkout] = useState({});
+  //const workout = this.props.
+  const [myWorkout, setMyWorkout] = useState(workout);
   const [secondsLeft, setSecondsLeft] = useState(60);
   const [exerName, setExerName] = useState("Quick Timer");
-  const [exerSets, setExerSets] = useState(0);
-  const [exerReps, setExerReps] = useState(0);
+  const [exerSets, setExerSets] = useState(workout.exercises.sets);
+  const [exerReps, setExerReps] = useState(workout.exercises.reps);
   const [timerOn, setTimerOn] = useState(false);
   const [intervalId, setIntervalId] = useState("null");
+  const [rest, setRest] = useState(false);
 
-  useEffect(async () => {
+  useEffect(() => {
+    console.log("initial Loading");
     const startWkout = async () => {
       // console.log(
       //   "addNewWorkout",
@@ -49,7 +79,7 @@ const Timer = () => {
       //     roundRest: 90,
       //   })
       // );
-      setMyWorkout(await getSingleWorkout("testworkout"));
+      // setMyWorkout(await getSingleWorkout("testworkout"));
       setExerName(myWorkout.exercises[0].name);
       setExerSets(myWorkout.exercises[0].sets);
       setExerReps(myWorkout.exercises[0].reps);
@@ -70,10 +100,43 @@ const Timer = () => {
   }, [timerOn]);
 
   useEffect(() => {
+    // if (myWorkout.exercises.length) {
+    // for (let i = 0; i < myWorkout.exercises.length; i++) {
+    // console.log(i, "im logging");
+    // if (i != 0) {
+    //   setExerName(myWorkout.exercises[i].name);
+    //   setExerSets(myWorkout.exercises[i].sets);
+    //   setExerReps(myWorkout.exercises[i].reps);
+    //   // setSecondsLeft();
+    // }
     if (secondsLeft === 0) {
-      setTimerOn(false);
+      if (exerSets != 0) {
+        if (rest) {
+          console.log("im resting");
+          setTimerOn(false);
+          setSecondsLeft(myWorkout.exercises[0].rest);
+          setRest(false);
+          setTimerOn(true);
+          // startTimer();
+        }
+        setExerSets(exerSets - 1);
+        setTimerOn(false); //this cleans up the initial interval preventing time from multiplying per second
+        setSecondsLeft(myWorkout.exercises[0].duration);
+        setTimerOn(true);
+        setTimerOn(false);
+        setRest(true);
+        setTimerOn(true);
+      } else if (myWorkout.exercises) {
+        setTimerOn(false);
+        setExerName(myWorkout.exercises[1].name);
+        setExerSets(myWorkout.exercises[1].sets);
+        setExerReps(myWorkout.exercises[1].reps);
+        setSecondsLeft(myWorkout.exercises[1].duration);
+      }
     }
   }, [secondsLeft]);
+  // }
+  // }, [secondsLeft]);
 
   const startTimer = () => {
     setIntervalId(
@@ -101,6 +164,15 @@ const Timer = () => {
     let displaySeconds = seconds < 10 ? `0${seconds}` : seconds;
     return { displayHours, displayMinutes, displaySeconds };
   };
+
+  //either call this on press of play the first time || give a begin workout button
+  // const beginWorkout = () => {
+  //   while (exerSets != 0) {
+  //     startTimer();
+  //     // startRest(); define this
+  //     setExerSets(exerSets - 1);
+  //   }
+  // };
 
   return (
     <KeyboardAvoidingView bg="colors.bg" height="100%">
